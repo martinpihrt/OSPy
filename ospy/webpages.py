3652a0dd5f6ab2fffa56bf17ee0db508455d1057
Martin Pihrt <martinpihrt@seznam.cz>
2022-03-25 16:35:47 +0100

    Fix plugin install. We do not recommend existing users to update their OSPy and plugins yet!!!

--- ospy/webpages.py
+++ ospy/webpages.py
@@ -1511,12 +1511,7 @@ class plugins_install_page(ProtectedPage):
             self._redirect_back()
 
         plugins.checker.update()
-
-        if options.plugin_readme_error: # true if is ImportError: Failed loading extension partial_gfm
-            errorCode = qdict.get('errorCode', 'gfm')
-        else:
-            errorCode = qdict.get('errorCode', 'none')
-        return self.core_render.plugins_install(errorCode)
+        return self.core_render.plugins_install()
 
 
     def POST(self):
